ef0d0447d95622493d28ee2277f05a2ef6da11e7
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-06-29 01:00:58 -0300

    feat: add cart button to product page

--- app/products/_components/product-details.tsx
+++ app/products/_components/product-details.tsx
@@ -137,6 +137,10 @@ const ProductDetails = ({
         <h3 className="font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
+      </div>
     </div>
   );
 };
