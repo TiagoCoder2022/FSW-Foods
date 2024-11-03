ebddc5ba61bc0b7f4fb05f8ae2ef66a0f300c4bc
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-11-02 23:36:51 -0300

    fix: add userFavoriteRestaurants prop to RestaurantItem

--- app/restaurants/recommended/page.tsx
+++ app/restaurants/recommended/page.tsx
@@ -1,9 +1,17 @@
 import Header from "@/app/_components/header";
 import RestaurantItem from "@/app/_components/restaurant-item";
+import { authOptions } from "@/app/_lib/auth";
 import { db } from "@/app/_lib/prisma";
+import { getServerSession } from "next-auth";
 
 const RecommendedRestaurants = async () => {
+  const session = await getServerSession(authOptions);
+
   const restaurants = await db.restaurant.findMany({});
+
+  const userFavoriteRestaurants = await db.userFavoriteRestaurants.findMany({
+    where: { userId: session?.user?.id },
+  });
   return (
     <>
       <Header hasSearchbar={true} />
@@ -15,6 +23,7 @@ const RecommendedRestaurants = async () => {
             <RestaurantItem
               key={restaurant.id}
               restaurant={restaurant}
+              userFavoriteRestaurants={userFavoriteRestaurants}
               className="min-w-full max-w-full"
             />
           ))}
